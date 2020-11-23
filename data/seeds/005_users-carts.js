a70ddadbf239b371af694cc8d3bbc41f6fc3cf44
rabah babaci <4rabah@gmail.com>
2020-11-22 22:24:04 -0800

    run users-carts migration and seeds

--- data/seeds/005_users-carts.js
+++ data/seeds/005_users-carts.js
@@ -6,8 +6,8 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('users-carts').insert([
         { profile_id: '00ulthapbErVUwVJy4x6', product_id: 1 },
-        { profile_id: '2j0epjkdhqo6wvkh0v7f', product_id: 2 },
-        { profile_id: '00ulthapbErVUwVJy4x6', product_id: 1 },
+        { profile_id: '2j0epjkdhqo6wvkh0v7f', product_id: 1 },
+        { profile_id: '00ulthapbErVUwVJy4x6', product_id: 2 },
         { profile_id: '2j0epjkdhqo6wvkh0v7f', product_id: 3 },
         { profile_id: '00ulthapbErVUwVJy4x6', product_id: 3 },
       ]);
