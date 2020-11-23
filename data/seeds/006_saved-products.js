a790090f90fd4a0be38f70fa0c8028f766a3f4b5
rabah babaci <4rabah@gmail.com>
2020-11-23 13:09:25 -0800

    create the saved-products seeds

--- data/seeds/006_saved-products.js
+++ data/seeds/006_saved-products.js
@@ -1,13 +1,15 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('saved-products')
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('saved-products').insert([
+        { profile_id: '2j0epjkdhqo6wvkh0v7f', product_id: 1 },
+        { profile_id: '00ulthapbErVUwVJy4x6', product_id: 3 },
+        { profile_id: '2j0epjkdhqo6wvkh0v7f', product_id: 2 },
+        { profile_id: '00ulthapbErVUwVJy4x6', product_id: 4 },
+        { profile_id: 'bv8tg9jvp9t1461zr7x1', product_id: 5 },
       ]);
     });
 };
