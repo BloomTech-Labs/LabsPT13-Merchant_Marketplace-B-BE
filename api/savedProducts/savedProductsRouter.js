31faf2fe8f61687bb7b0e1eb015482d646b2e6de
rabah babaci <4rabah@gmail.com>
2020-11-23 13:14:47 -0800

    define the saveProducts router

---
+++ api/savedProducts/savedProductsRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authRequired = require('../middleware/authRequired');
+const validateBody = require('../middleware/validateBody');
+
+const { findBy, findAll, remove } = require('../globalDbModels');
+
+router.get('/', authRequired, async (req, res) => {
+  const savedProducts = await findAll('saved-products');
+  res.status(200).json(savedProducts);
+});
+
+module.exports = router;
