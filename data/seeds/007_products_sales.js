59c76f83557ab972981192a67ea1e0e214f9ec6e
rabah babaci <4rabah@gmail.com>
2020-12-02 11:33:36 -0800

    fix products_sales seed table typo

--- data/seeds/007_products_sales.js
+++ data/seeds/007_products_sales.js
@@ -1,14 +1,13 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('product_sales')
+  return knex('products_sales')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('product_sales').insert([
-        { product_id: 1, starting_quantity: 0, num_sales: 0 },
+      return knex('products_sales').insert([
+        { product_id: 1, starting_quantity: 2, num_sales: 0 },
         { product_id: 2, starting_quantity: 8, num_sales: 4 },
-
-        { product_id: 3, starting_quantity: 0, num_sales: 2 },
+        { product_id: 3, starting_quantity: 3, num_sales: 2 },
       ]);
     });
 };
