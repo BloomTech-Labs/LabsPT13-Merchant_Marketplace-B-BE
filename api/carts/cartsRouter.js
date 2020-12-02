6651e400525cd536fd486d6522f31e6e20ad3392
Rabah Babaci <4rabah@gmail.com>
2020-12-01 22:12:54 -0800

    remove the unused findAll method

--- api/carts/cartsRouter.js
+++ api/carts/cartsRouter.js
@@ -4,7 +4,6 @@ const authRequired = require('../middleware/authRequired');
 const validateBody = require('../middleware/validateBody');
 const {
   findProfileItems,
-  findAll,
   findBy,
   create,
   remove,
