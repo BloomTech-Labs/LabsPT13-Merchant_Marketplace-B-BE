263ee63d35323df7f567f2bd6b165b354e7d5861
rabah babaci <4rabah@gmail.com>
2020-12-17 17:32:15 -0800

    install express file upload

--- data/migrations/20201217135530_create-products-images.js
+++ data/migrations/20201217135530_create-products-images.js
@@ -1,8 +1,16 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('products-images', function (table) {
+    table
+      .integer('product_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('products')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('products-images');
 };
