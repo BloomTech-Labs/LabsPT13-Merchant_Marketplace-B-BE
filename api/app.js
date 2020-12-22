4ff2637869b42c2ea0c20f99629dc8ce009196a0
rabah babaci <4rabah@gmail.com>
2020-12-22 13:11:18 -0800

    install and configure cloudinary

--- api/app.js
+++ api/app.js
@@ -43,14 +43,14 @@ app.use(
 );
 
 app.use(helmet());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(
   cors({
     origin: '*',
   })
 );
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(fileUpload());
 
