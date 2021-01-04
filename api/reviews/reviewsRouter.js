9eb9ecdd5deff79c4863c300d23805cbe26795fb
rabah babaci <4rabah@gmail.com>
2021-01-04 12:04:24 -0800

    create the get seller reviews endpoint

--- api/reviews/reviewsRouter.js
+++ api/reviews/reviewsRouter.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const authRequired = require('../middleware/authRequired');
 const validateId = require('../middleware/validateId');
 const validateBody = require('../middleware/validateBody');
-const { findAll, findBy, create, remove } = require('../globalDbModels');
+const { findAllBy, findBy, create, remove } = require('../globalDbModels');
 
 const TABLE_NAME = 'reviews';
 
 // retrieve single seller's reviews
 router.get('/:id', authRequired, validateId('profiles'), async (req, res) => {
-  res.status(200).json({ message: 'MADE IT' });
+  try {
+    const { id } = req.params;
+    const reviews = await findAllBy(TABLE_NAME, { seller_id: id });
+
+    res.status(200).json(reviews);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // create a new review
